2d40ba5fd876af84a7b7a1348aaeb252bc264bf8
Jason Salem <jason.salem19@hotmail.com>
2021-09-20 11:38:11 +0300

    Upload Files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Salex Webserver
  
+# Under development 
---
+++ testing/device.py
@@ -0,0 +1,11 @@
+device = {
+    "name": "Jason's iPhone",
+    "vendor": "Apple",
+    "version": "IOS 14.5",
+    "IP Address": "192.168.1.115"
+}
+
+print("-------------DEVICE INFO-------------")
+for key, value in device.items():
+    print(f"{key:>16s} : {value}")
+
